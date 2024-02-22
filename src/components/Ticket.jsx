6acc6b67db340b0e69c2faed22dd9131619929ad
NicoDipi <nicolas.dipierro@7r1ck.com>
2024-02-22 03:35:24 -0300

    *Warnings fixed, first interation of longer ticket name autoscroll animation

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,29 +1,49 @@
-import { Reorder } from "framer-motion";
+import { Reorder, motion } from "framer-motion";
 import { Icon } from "@iconify/react";
 
 import { useTicketHandle } from "../hooks/useTicketHandle.js"
 import { TICKET_VARIANTS } from "../utils/ticketStates.js";
+import { useEffect, useRef, useState } from "react";
 
 const Ticket = ({ticket}) =>
 {
-	const {name, price, amount } = ticket;
+	const nameElement = useRef(null);
 
+	const {name, price, amount } = ticket;
 	const { state, draggeable, handleSpent, handleDrag } = useTicketHandle({ticket});
+
+	useEffect(()=>{
+
+		if(nameElement.current && nameElement.current.scrollWidth > nameElement.current.clientWidth)
+		{
+			console.log("Ticket with a longer name");
+		}
+
+	},[])
 
 	return (
 		<Reorder.Item
 			value={ticket}
+
+			initial={TICKET_VARIANTS.normal}
+			animate={state}
+			variants={TICKET_VARIANTS}
+			
 			drag
 			dragDirectionLock
 			dragConstraints={{left:0,right:0}}
-			dragListener={draggeable}	
+			dragListener={draggeable}
 			onDragEnd={handleDrag}
-			animate={state}
-			variants={TICKET_VARIANTS}
+			
 			className="list-ticket"
 		>
 			<div className="list-ticket-section">
-				<p>{name}</p>
+				<motion.p 
+					ref={nameElement} 
+					className="list-ticket-name"
+				>
+					{name}
+				</motion.p>
 				<p className="list-ticket-amount">x{amount}</p>
 			</div>
 			<div className="list-ticket-section">
